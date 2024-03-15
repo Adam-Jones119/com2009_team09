143bde007e93bb51f2787064449eef45791714f5
Jerry Brophy <jerrybrophy1@gmail.com>
2024-03-15 09:55:51 +0000

    Randomised turn direction when LiDar detects object in range. Added logic for turning left and right

--- src/avoiding_obstacles.py
+++ src/avoiding_obstacles.py
@@ -3,6 +3,7 @@
 import rospy
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Bool
+import random
 
 class MovementPublisher:
     def __init__(self):
@@ -10,9 +11,11 @@ class MovementPublisher:
         self.publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         self.obstacle_subscriber = rospy.Subscriber('/obstacle_detected', Bool, self.obstacle_callback)
         self.rate = rospy.Rate(10)  # 10 Hz
-        self.linear_speed = 0.2  # Adjust as needed
-        self.angular_speed = 0.5  # Adjust as needed
-        self.obstacle_detected = False
+        self.linear_speed = 0.2 
+        self.angular_speed = 0.5 
+        self.obstacle_detected = False #initialise turning logic for when an obstacle is detected.
+        self.turn_direction = "left" 
+        self.turning = False
 
     def obstacle_callback(self, msg):
         self.obstacle_detected = msg.data
@@ -22,10 +25,18 @@ class MovementPublisher:
         if not self.obstacle_detected:
             twist.linear.x = self.linear_speed
             twist.angular.z = 0
+            self.turning = False
         else:
-            twist.linear.x = 0  # Stop forward motion
-            twist.angular.z = self.angular_speed  # Rotate until the obstacle is no longer in front
+            if not self.turning:
+                self.turn_direction = random.choice(["left", "right"]) #randomises turning left or right
+                self.turning = True
+                twist.linear.x = 0  # Stop forward motion
+            if self.turn_direction == "left":
+                twist.angular.z = self.angular_speed
+            else:
+                twist.angular.z = -self.angular_speed
         self.publisher.publish(twist)
+        rospy.sleep(0.5)
 
 if __name__ == '__main__':
     try:
